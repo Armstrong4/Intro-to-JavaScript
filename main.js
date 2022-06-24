d802a2b6c95deabc02ef0e227c3cdb3a3e0948d6
Armstrong4 <armst.amdi4@gmail.com>
2022-06-24 14:52:37 +0100

    Initial commit

---
+++ main.js
@@ -0,0 +1,5 @@
+/*The submission mode wasn't so clear about which screen to print out on so I thought to err on the side of caution.*/
+
+console.log('My name is Ikwechegh, Armstong. \nI am five feet six inches tall. \nI am Nigerian.');
+document.write('My name is Ikwechegh, Armstong. I am five feet six inches tall. I am Nigerian.');
+alert('My name is Ikwechegh, Armstong. \nI am five feet six inches tall. \nI am Nigerian.');
